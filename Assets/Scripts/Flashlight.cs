b49aa8bc8c09ab9110cdb1cba2d4427cb5d27990
Eduardo Sarmento <cealvesarmento@gmail.com>
2019-06-25 16:38:48 -0300

    Environment improved
    - Night ambient
    - Maze size changed
    - Flashlight created

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Light))]
+public class Flashlight : MonoBehaviour  
+{
+    public float charge;
+    Light flashlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashlight = GetComponent<Light>();
+        flashlight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1") && charge > 0)
+            flashlight.enabled = !flashlight.enabled;
+
+        if(flashlight.enabled)
+        {
+            charge -= Time.deltaTime;
+
+            if (charge <= 0)
+                flashlight.enabled = false;
+        }        
+    }
+}
